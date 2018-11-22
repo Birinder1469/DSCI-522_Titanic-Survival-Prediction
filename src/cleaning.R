7ddf0d990439e7295dc8e01a6d24135bc73eb4d1
Alden Chen <alden.chen@outlook.com>
2018-11-22 08:51:24 -0800

    Add command line arguments and main function

--- src/cleaning.R
+++ src/cleaning.R
@@ -1,28 +1,34 @@
 library(tidyverse)
 
-titanic_data <- read_csv("data/train.csv")
+# read in command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+input_file <- args[1]
+output_file <- args[2]
 
-# cleaning titanic data to get numerical variables for
-# embarked and gender.
-# drop cases where there is an na for a feature
-titanic_data <- titanic_data %>% 
-  mutate(Embarked = as.factor(Embarked),
-         Sex = as.factor(Sex)) %>% 
-  mutate(Embarked = fct_recode(titanic_data$Embarked, 
-                    "1" = "C",
-                    "2" = "Q",
-                    "3" = "S"),
-         Sex = fct_recode(titanic_data$Sex,
-                          "1" = "male",
-                          "0" = "female")) %>% 
-  filter(is.na(Embarked) == FALSE,
-         is.na(Age) == FALSE,
-         is.na(Pclass) == FALSE,
-         is.na(Sex) == FALSE,
-         is.na(SibSp) == FALSE,
-         is.na(Parch) == FALSE,
-         is.na(Survived) == FALSE)
+# input_file <- read_csv("data/train.csv")
+# output_file <- "data/titanic.csv"
 
+# main function for cleaning titanic data
+main <- function(){
+  titanic_data <- input_file %>% 
+    mutate(Embarked = as.factor(Embarked),
+           Sex = as.factor(Sex)) %>% 
+    mutate(Embarked = fct_recode(input_file$Embarked, 
+                      "1" = "C",
+                      "2" = "Q",
+                      "3" = "S"),
+           Sex = fct_recode(input_file$Sex,
+                            "1" = "male",
+                            "0" = "female")) %>% 
+    filter(is.na(Embarked) == FALSE,
+           is.na(Age) == FALSE,
+           is.na(Pclass) == FALSE,
+           is.na(Sex) == FALSE,
+           is.na(SibSp) == FALSE,
+           is.na(Parch) == FALSE,
+           is.na(Survived) == FALSE)
+  write_csv(titanic_data, output_file)
+  }
 
-write_csv(titanic_data, "data/titanic_data.csv")
-
+main()
+  
