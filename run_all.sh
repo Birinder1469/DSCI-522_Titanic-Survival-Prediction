f8e27fe199a8494d8c94a2d02d2523778a4c65a4
Birinder <samdsilva1469@gmail.com>
2018-11-23 12:48:07 -0800

    Added the RMD file name to the run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -4,8 +4,12 @@ Rscript src/cleaning.R 'data/train.csv' 'data/titanic_data.csv'
 # Exploratory data analysis
 Rscript src/eda.R 'data/titanic_data.csv' 'results'
 
-# 5-fold cross validation to 
+# 5-fold cross validation to
 python src/hyperparameter-tuning.py 'data/titanic_data.csv' 'data' 'results'
 
 # Fit decision tree and generate plot of tree and feature importances
 python src/fit-decision-tree.py 'data' 'results'
+
+
+# Write the report
+Rscript -e "rmarkdown::render('doc/report_titanic-predictors.Rmd')"
