60f2a0e09569cdf15edeac6a651d4a103ed3af81
Alden Chen <alden.chen@outlook.com>
2018-11-23 13:09:51 -0800

    Fix axis labels on graphs

--- src/eda.R
+++ src/eda.R
@@ -2,6 +2,7 @@
 ## some plots for EDA
 
 library(tidyverse)
+library(grid)
 
 args = commandArgs(trailingOnly = TRUE)
 input = args[1]
@@ -11,6 +12,7 @@ output = args[2]
 main <- function(){
   
   require(tidyverse)
+  require(grid)
   titanic_data <- read_csv(input)
   
   titanic_data <- titanic_data %>% 
@@ -19,43 +21,47 @@ main <- function(){
            Embarked = as.factor(Embarked),
            Pclass = as.factor(Pclass)) 
   
+  titanic_data <- titanic_data %>% 
+    mutate(Survived = fct_recode(titanic_data$Survived,
+                      "Survived" = "1",
+                      "Did not Survive" = "0"),
+           Sex = fct_recode(titanic_data$Sex,
+                            "Female" = "0",
+                            "Male" = "1"))
+  
   boxplot_age <- titanic_data %>% 
     ggplot(aes(x = Survived, y = Age)) +
     geom_boxplot() +
     geom_jitter(alpha = 0.2) +
-    xlab("Survived") +
-    ggtitle("Survival vs Age")
+    ggtitle("Survival vs Age") +
+    xlab(" ")
   
   boxplot_fare <- titanic_data %>% 
     ggplot(aes(x = Survived, y = Fare)) +
     geom_boxplot() +
     geom_jitter(alpha = 0.2) +
     ylim(0, 135) +
-    xlab("Survived") +
-    ggtitle("Survival vs Fare")
+    ggtitle("Survival vs Fare") +
+    xlab(" ")
   
   barplot_survival <- titanic_data %>% 
     ggplot(aes(x = Survived)) +
     geom_bar() +
-    xlab("Survived") +
     ggtitle("Survival")
   
   barplot_sex_survival <- titanic_data %>%
-    mutate(Survived = fct_recode(titanic_data$Survived,
-                                 "Survived" = "1",
-                                 "Did not Survive" = "0")) %>% 
     ggplot(aes(x = Survived)) +
     geom_bar() +
     facet_wrap(~Sex) +
-    xlab("Survived") +
-    ggtitle("Survival by Sex")
+    ggtitle("Survival by Sex") +
+    xlab(" ")
   
   barplot_class_survival <- titanic_data %>% 
     ggplot(aes(x = Survived)) +
     geom_bar() +
     facet_wrap(~Pclass) +
-    xlab("Survived") +
-    ggtitle("Survival by Cabin Class")
+    ggtitle("Survival by Cabin Class") +
+    xlab(" ")
   
   barplot_port_survival <- titanic_data %>% 
     mutate(Embarked = fct_recode(titanic_data$Embarked, 
@@ -65,9 +71,10 @@ main <- function(){
     ggplot(aes(x = Survived)) +
     geom_bar() +
     facet_wrap(~Embarked) +
-    xlab("Survived") +
-    ggtitle("Survival by Embarkation Port")
+    ggtitle("Survival by Embarkation Port")+
+    xlab(" ")
   
+
   ggsave("eda_boxplot-age-survival.png", plot = boxplot_age, device = "png",
          path = output)
   ggsave("eda_boxplot-fare-survival.png", plot = boxplot_fare, device = "png",
@@ -80,6 +87,7 @@ main <- function(){
          path = output)
   ggsave("eda_barplot-port-survival.png", plot = barplot_port_survival, device = "png",
          path = output)
+
   
   }
 
