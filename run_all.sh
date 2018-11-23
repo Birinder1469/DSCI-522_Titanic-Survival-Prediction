beec6db5aeb94cbbf79c9214550a5c2117c6ea43
Alden Chen <alden.chen@outlook.com>
2018-11-23 09:43:13 -0800

    Completed shell script to run analysis

--- run_all.sh
+++ run_all.sh
@@ -1,2 +1,11 @@
-# Execute the Decisopn Tree Classifier algorithm
-python src/decisiontree-execution.py 'data' 'results'
+# Load and clean data
+Rscript src/cleaning.R 'data/train.csv' 'data/titanic_data.csv'
+
+# Exploratory data analysis
+Rscript src/eda.R 'data/titanic_data.csv' 'results'
+
+# 5-fold cross validation to 
+python src/hyperparameter-tuning.py 'data/titanic_data.csv' 'data' 'results'
+
+# Fit decision tree and generate plot of tree and feature importances
+python src/fit-decision-tree.py 'data' 'results'
