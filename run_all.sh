ad0b83873fddb85a21bc11f8fdfd6ced0edfff88
Alden Chen <alden.chen@outlook.com>
2018-11-23 12:36:41 -0800

    Update shell script to knit report

--- run_all.sh
+++ run_all.sh
@@ -9,3 +9,7 @@ python src/hyperparameter-tuning.py 'data/titanic_data.csv' 'data' 'results'
 
 # Fit decision tree and generate plot of tree and feature importances
 python src/fit-decision-tree.py 'data' 'results'
+
+# Report
+Rscript -e "rmarkdown::render('doc/report_titanic-predictors.Rmd')"
+
